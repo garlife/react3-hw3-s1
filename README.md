c4aeb92338d9bbca33d6b55e32061f2110df6170
garlife <garlife@gmail.com>
2022-01-25 00:35:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yqgmpt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yqgmpt)
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Card, Elevation } from '@blueprintjs/core';
+
+function Tick() {
+  ReactDOM.render(
+    <Card
+      interactive={true}
+      elevation={Elevation.TWO}
+      style={{
+        maxWidth: '50%',
+        textAlign: 'center',
+        marginLeft: '10%',
+        marginTop: '10%',
+        float: 'left',
+        border: 'round solid',
+      }}
+    >
+      <h5>
+        <a href="#">Timer is running every 30 sec</a>
+      </h5>
+      <h3>Timer: {new Date().toLocaleTimeString()}</h3>
+    </Card>,
+    document.getElementById('root')
+  );
+}
+Tick();
+setInterval(Tick, 30000);
